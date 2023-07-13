3f3ea7472d1f63ed24e2b21f72576a3a71f2504d
farhan hossain <farhanrufon99@gmail.com>
2023-07-13 16:51:43 -0400

    modifies index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const newTask = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description")
+  const newUlTask = document.getElementById("tasks");
+  newTask.addEventListener('submit', (e) => {
+    e.preventDefault();
+    taskDescription.value
+    const newList = document.createElement('li')
+    newList.textContent = taskDescription.value 
+    newUlTask.append(newList)
+  })  
+
+  
+
 });
+
+
